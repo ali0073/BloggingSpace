c8dba7c8b139d47f381029e07bb7f77193ed051e
Ali Ghazanfar <alieghazanfar@gmail.com>
2023-08-16 12:27:40 +0500

    Remove empty lines

--- src/network/axiosClient.js
+++ src/network/axiosClient.js
@@ -8,7 +8,6 @@ axiosClient.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}`;
 axiosClient.interceptors.response.use( response => {
   return response;
 }, error => {
-
   if (error.response.status === 500) {
     toast.error("Something went wrong, please try again!");
   } 
